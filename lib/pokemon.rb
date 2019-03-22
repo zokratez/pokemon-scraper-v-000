5c22fc752f80d3fc885aa104369b002ce305a142
Sam Oteo <tortillabar@me.com>
2019-03-22 18:41:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,35 @@
+require 'pry'
+
 class Pokemon
   
-  attr_accessor :name, :type, :db
+  attr_accessor :name, :type, :db, :id
   
-  def initialize(name:, type:, db:)
+  @@all = []
+  
+  def initialize (name:, type:, db:, id:)
     @name = name
     @type = type
     @db = db
+    @id = id
+   #@all = self
+  end
+  
+  
+  def self.save (name, type, db)   # instance method
+    db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
-
+  def self.find (id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+ #binding.pry
+    self.new()
+    
+    
+ 
+    
+  end
+  
+  
+#name - pokemon
 
 end
